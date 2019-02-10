5ceeff9297260a53dbeef8363b8d809682f54df3
Gazizova-Olga <gazizova.or@gmail.com>
2019-02-10 14:37:34 +0300

    game is added

---
+++ Snake/VerticalLine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine :Figure
+    {
+
+        public VerticalLine(int yHigh, int yLow, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yHigh; y <= yLow; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+    }
+}
